fc5285d3ae6d1041cff64023edce0b5df288ca5a
fclmail <fclmail3@mail.com>
2025-11-18 19:47:30 -0500

    Update arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -55,7 +55,6 @@ const arbContract = new ethers.Contract(CONTRACT_ADDRESS, arbAbi, provider);
 const routers = {
   QuickSwap: "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff",
   SushiSwap: "0x1b02da8cb0d097eb8d57a175b88c7d8b47997506",
-  Dfyn: "0xa8b607aa09b6a2641cf690f643e76d3f6e6ff73",
   ApeSwap: "0xC0788A3aD43d79aa53B09c2EaCc313A787d1d607"
 };
 
