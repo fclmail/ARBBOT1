8f2d8b6aaa0d8427efacb9b5ca9207458ea8bd8f
fclmail <fclmail3@mail.com>
2025-12-04 13:22:21 -0500

    Update arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -27,7 +27,7 @@ const routers = {
 const tokens = {
   WETH: "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619",
   WBTC: "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6",
-  CRV:  "0x172370D5Cd63279eFa6d502Dab29171933a610AF"
+  CRV:  "0x172370d5Cd63279eFa6d502Dab29171933a610AF"
 };
 
 // ---------------- ABI ----------------
