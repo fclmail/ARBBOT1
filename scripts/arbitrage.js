a19a8ca12cafe3604762f0912372df86c98f6d10
fclmail <fclmail3@mail.com>
2025-12-08 23:48:11 -0500

    Update arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -20,7 +20,7 @@ const MIN_TRADE_USDC = 0.01;
 const MIN_EXPECTED_PROFIT = 0.001;
 const SLIPPAGE_PCT = 0.0;
 const MAX_PROFIT_PCT = 40;
-const TRADE_AMOUNT_USDC = 0.01;
+const TRADE_AMOUNT_USDC = 0.1;
 
 // Routers and Tokens
 const routers = {
