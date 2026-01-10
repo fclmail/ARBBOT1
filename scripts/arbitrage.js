2cb6670ce8f1cd10795b7ef0408b58f3db9eefa7
fclmail <fclmail3@mail.com>
2026-01-09 20:12:19 -0500

    Update arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -22,7 +22,7 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const provider = new ethers.JsonRpcProvider(RPC);
 const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
 
-const MIN_TRADE_USDC = 0.03;
+const MIN_TRADE_USDC = 0.030;
 const MIN_PROFIT_USDC = 0.00001;
 const MIN_PROFIT_PCT = 0.3;
 const SLIPPAGE_PCT = 0.5;
