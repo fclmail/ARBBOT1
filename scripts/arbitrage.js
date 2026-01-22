f0203d8fe8643240d4e264339ec044b25212f07e
fclmail <fclmail3@mail.com>
2026-01-22 17:09:02 -0500

    Update arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -56,8 +56,8 @@ const vault = new ethers.Contract(VAULT_ADDRESS, vaultAbi, wallet);
 
 /* ================= ROUTERS ================= */
 const routers = {
-   CafeSwap:   "0x9055682E58C74fc8DdBFC55Ad2428aB1F96098Fc",
-       DFYN:       "0xa102072a4c07f06ec3b4900fdc4c7b80b6c57429",
+  // CafeSwap:   "0x9055682E58C74fc8DdBFC55Ad2428aB1F96098Fc",
+//       DFYN:       "0xa102072a4c07f06ec3b4900fdc4c7b80b6c57429",
     ApeSwap:    "0xC0788A3aD43d79aa53B09c2EaCc313A787d1d607",
   SushiSwap:  "0x1b02da8cb0d097eb8d57a175b88c7d8b47997506",
   QuickSwap:  "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff",
