086cb5095c0391d5fba204443737a53d1d861aa3
fclmail <fclmail3@mail.com>
2026-02-04 19:28:56 -0500

    Update arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -34,7 +34,7 @@ const ERC20_ABI = [
 
 // ================= ROUTERS & PATHS =================
 const ROUTERS = [
-  "0xa5E0829CaCED8fFDD4De3c43696c57F7D7A678ff", // QuickSwap
+ // "0xa5E0829CaCED8fFDD4De3c43696c57F7D7A678ff", // QuickSwap
   "0x1b02da8cb0d097eb8d57a175b88c7d8b47997506", // SushiSwap
   "0xC0788A3aD43d79aa53B09c2EaCc313A787d1d607", // ApeSwap
   "0xA102072A4C07F06EC3B4900FDC4C7B80b6C57429"  // Dfyn
