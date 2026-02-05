271a81dbeb1bccf48bcf87a94fb21acb0ae041b8
fclmail <fclmail3@mail.com>
2026-02-05 16:57:18 -0500

    Update arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -35,7 +35,7 @@ const DEADLINE_SECONDS = 60;
 
 /* ================= AUTO-WITHDRAW SETTINGS ================= */
 
-const AUTO_WITHDRAW_THRESHOLD_USDC = 2.0; // trigger threshold
+const AUTO_WITHDRAW_THRESHOLD_USDC = 1.0; // trigger threshold
 let AUTO_WITHDRAW_PERCENT = 5; // 5% of vault, adjustable 1-100%
 
 /* ================= PROVIDER ================= */
@@ -77,7 +77,7 @@ const vault = new ethers.Contract(VAULT_ADDRESS, vaultAbi, wallet);
 
 const routers = {
   QuickSwap: "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff",
-  Dfyn: "0xA8b607Aa09B6A2641cF6F90f643E76d3f6e6Ff73",
+  Wault:     "0xa98ea6356a316b44bf710d5f9b6b4ea0081409ef",
   SushiSwap: "0x1b02da8cb0d097eb8d57a175b88c7d8b47997506",
   ApeSwap: "0xC0788A3aD43d79aa53B09c2EaCc313A787d1d607"
 };
