963e719edb0a31fc863a19f3c522f2957476c123
fclmail <fclmail3@mail.com>
2026-02-14 13:01:57 -0500

    Update arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -177,7 +177,7 @@ async function findOptimalFlashAmount(
   buyPath,
   sellPath
 ) {
-  const multipliers = [1n, 2n, 4n, 8n, 12n, 13n, 14n, 15n, 16n, 32n, 64n, 128n, 256n, 512n, 1024n, 2048n, 4096n, 8092n];
+  const multipliers = [1n, 2n, 4n, 8n, 12n, 14n,16n, 32n, 64n, 128n, 256n, 512n, 1024n, 2048n, 4096n, 8092n];
   let bestAmount = baseAmount;
   let bestProfit = 0n;
 
