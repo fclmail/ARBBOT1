644befddf2f9506f8a738020cacb80d6138758fa
fclmail <fclmail3@mail.com>
2026-02-19 09:59:34 -0500

    Update arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -31,7 +31,7 @@ const YELLOW = "\x1b[93m";
 /* ================= CONSTANTS ================= */
 
 // SMART CONTRACT: minimum profit = 1 = 0.000001 USDCe
-const MIN_TRADE_USDC = 1.7;
+const MIN_TRADE_USDC = .02;
 const MIN_EXPECTED_PROFIT = 0.000001;
 
 const SCAN_INTERVAL_MS = 10_000;
@@ -39,8 +39,8 @@ const DEADLINE_SECONDS = 60;
 
 /* ================= WITHDRAW ================= */
 
-const WITHDRAW_THRESHOLD_USDC = 1;
-const WITHDRAW_PERCENT = 100;
+const WITHDRAW_THRESHOLD_USDC = .57;
+const WITHDRAW_PERCENT = 1;
 
 /* ================= PROVIDER ================= */
 
