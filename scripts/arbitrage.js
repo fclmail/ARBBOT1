07b2aab235639712f6a965e553e38893e31d79ce
fclmail <fclmail3@mail.com>
2026-02-23 02:37:17 -0500

    Update WITHDRAW_THRESHOLD_USDC value

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -35,7 +35,7 @@ const MIN_EXPECTED_PROFIT = 0.000001;
 const SCAN_INTERVAL_MS = 10_000;
 const DEADLINE_SECONDS = 60;
 
-const WITHDRAW_THRESHOLD_USDC = 2;
+const WITHDRAW_THRESHOLD_USDC = 7875554;
 const WITHDRAW_PERCENT = 1;
 
 /* ================= PROVIDER ================= */
