c27a8fb0d480a07c84e26645ffc5b1a9ae90014d
fclmail <fclmail3@mail.com>
2026-02-23 11:31:55 -0500

    Change MIN_TRADE_USDC to 0.02

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -1,3 +1,4 @@
+J's v c m🔃📶💰
 
 import dotenv from "dotenv";
 import { ethers } from "ethers";
@@ -30,7 +31,7 @@ const RED = "\x1b[91m";
 
 /* ================= CONSTANTS ================= */
 
-const MIN_TRADE_USDC = 1000;
+const MIN_TRADE_USDC = 0.02;
 const MIN_EXPECTED_PROFIT = 0.000001;
 
 const SCAN_INTERVAL_MS = 10_000;
