7a5f4762190120e67c1700e8d227a5f9d2ae8d6a
fclmail <fclmail3@mail.com>
2026-03-04 22:21:43 -0500

    Update arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -39,8 +39,8 @@ const DEADLINE_SECONDS = 60;
 
 /* ================= WITHDRAW ================= */
 
-const WITHDRAW_THRESHOLD_USDC = 1;
-const WITHDRAW_PERCENT = 100;
+const WITHDRAW_THRESHOLD_USDC = 10;
+const WITHDRAW_PERCENT = 1;
 
 /* ================= PROVIDER ================= */
 
