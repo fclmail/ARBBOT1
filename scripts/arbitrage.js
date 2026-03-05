1b84449d64a1d22fa1469108a896dcb90b67877e
fclmail <fclmail3@mail.com>
2026-03-04 22:15:08 -0500

    Update arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -37,10 +37,10 @@ if (!WALLET_PRIVATE_KEY) throw new Error("WALLET_PRIVATE_KEY is missing or empty
 /* ================= CONSTANTS ================= */
 
 // 🟢7 TRADE SETTINGS (UNCHANGED)
-const MIN_TRADE_USDC = .03;
-const MIN_EXPECTED_PROFIT = 0.000001;
+const MIN_TRADE_USDC = 1.7;
+const MIN_EXPECTED_PROFIT = 0.0000001;
 const SLIPPAGE_PCT = 0.05;
-const SCAN_INTERVAL_MS = 10_000; // ✅ HARD 10 SECOND SCAN
+const SCAN_INTERVAL_MS = 1_000; // ✅ HARD 10 SECOND SCAN
 const DEADLINE_SECONDS = 60;
 
 /* ================= PROVIDER ================= */
@@ -89,6 +89,7 @@ const vault = new ethers.Contract(VAULT_ADDRESS, vaultAbi, wallet);
 // 🟢13 DEX ROUTERS
 const routers = {
   QuickSwap: "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff",
+  Dfyn: "0xA8b607Aa09B6A2641cF6F90f643E76d3f6e6Ff73",
   SushiSwap: "0x1b02da8cb0d097eb8d57a175b88c7d8b47997506",
   ApeSwap:   "0xC0788A3aD43d79aa53B09c2EaCc313A787d1d607"
 };
@@ -104,6 +105,14 @@ const routerAbi = [
 const TOKENS = {
   USDT: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
   WBTC: "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6",
+   APE:"0x4d224452801aced8b2f0aebe155379bb5d594381",
+   CRV:"0x172370d5cd63279efa6d502dab29171933a610af",
+   DAI:"0x8f3cf7ad23cd3cadbd9735aff958023239c6a063",
+   MATICX:"0xa3fa99a148fa48d14ed51d610c367c61876997f1",
+   UNI:"0x1f9840a85d5af5bf1d1762f925bdaddc4201f984",
+   UNI2:"0xb33eaad8d922b1083446dc23f610c2567fb5180f",
+   WMATIC:"0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270",
+    WETH:"0x7ceb23fd6bc0add59e62ac25578270cff1b9f619",
   LINK: "0x53e0bca35ec356bd5dddfebbd1fc0fd03fabad39",
   AAVE: "0xd6df932a45c0f255f85145f286ea0b292b21c90b"
 };
