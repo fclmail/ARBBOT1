48898e69846e99e794e13fc5954f00b58c0f95f7
fclmail <fclmail3@mail.com>
2026-03-04 20:35:08 -0500

    Update arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -17,7 +17,7 @@ if (!PRIVATE_KEY) throw new Error("WALLET_PRIVATE_KEY not found.");
 
 const DRY_RUN = false;
 const MIN_TRADE_USDC = .45;
-const MIN_EXPECTED_PROFIT = 0.000001;
+const MIN_EXPECTED_PROFIT = 1n;
 const MIN_PROFIT_PCT = 0.001;
 const SLIPPAGE_PCT = 0.05;
 const MAX_PROFIT_PCT = 550;
