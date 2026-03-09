c0ca2c7bcde81f63d4498385ae2bbdb2c6d48490
fclmail <fclmail3@mail.com>
2026-03-09 05:06:52 -0400

    Update arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -30,7 +30,7 @@ const RED = "\x1b[91m";
 const MIN_TRADE_USDC = .02;
 const MIN_EXPECTED_PROFIT = 0.000001;
 
-const SCAN_INTERVAL_MS = 10_000;
+const SCAN_INTERVAL_MS = 3_000;
 const DEADLINE_SECONDS = 6000;
 const MAX_BATCH_SIZE = 10;
 
