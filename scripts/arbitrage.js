be0794643f11608a50ea6c38dc5db74691dd6466
fclmail <fclmail3@mail.com>
2026-03-13 14:19:44 -0400

    Update arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -21,10 +21,10 @@ const RED = "\x1b[91m";
 
 /* ================= CONSTANTS ================= */
 const MIN_TRADE_USDC = 0.02; // micro trade amount
-const MIN_EXPECTED_PROFIT = 0.000001; // minimum profit per micro trade
+const MIN_EXPECTED_PROFIT = 0.00001; // minimum profit per micro trade
 const SCAN_INTERVAL_MS = 10000;
 const DEADLINE_SECONDS = 6000;
-const TARGET_BATCH_SIZE = 10; // execute batch when reached
+const TARGET_BATCH_SIZE = 20; // execute batch when reached
 const WORKERS = 16;
 
 /* ================= TRADE BUFFER ================= */
