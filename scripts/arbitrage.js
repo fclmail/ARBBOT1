e4fc29223031d5bf5831bd3d9aab27616fa0fd51
fclmail <fclmail3@mail.com>
2026-03-13 13:31:32 -0400

    Update arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -35,7 +35,7 @@ const provider = new ethers.JsonRpcProvider(RPC_POLYGON);
 const wallet = new ethers.Wallet(WALLET_PRIVATE_KEY, provider);
 
 /* ================= CONTRACT ================= */
-const VAULT_ADDRESS = "0xf2F8e22D4A8F0a546fe0c42FfFC2cdCc6F9c827f";
+const VAULT_ADDRESS = "0xf7e8A1580Dd9b3757Fb6a1f86AD5ed0e0F3EfC31";
 
 const vaultAbi = [
   {
