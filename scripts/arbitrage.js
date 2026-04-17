6afb5270359f3469f2f9db4795cfd20238808dd4
fclmail <fclmail3@mail.com>
2026-04-17 14:09:30 -0400

    Update MIN_BATCH_PROFIT to 0.001

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -32,7 +32,7 @@ let routerContracts;
 const TRADE_AMOUNT = ethers.parseUnits("0.01", 6);
 const MIN_PROFIT = ethers.parseUnits("0.0003", 6);
 
-const MIN_BATCH_PROFIT = ethers.parseUnits("0.01", 6);
+const MIN_BATCH_PROFIT = ethers.parseUnits("0.001", 6);
 const SAFETY_MULTIPLIER = 190n;
 
 const SAFE_BATCH_TRIGGER =
