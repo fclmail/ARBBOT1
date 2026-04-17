6bb3b90fbd6fe96a5914a5eb7a9457db3325332a
fclmail <fclmail3@mail.com>
2026-04-17 10:56:22 -0400

    Increase MIN_PROFIT threshold in arbitrage script

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -30,7 +30,7 @@ let routerContracts;
 /* ================= CONFIG ================= */
 
 const TRADE_AMOUNT = ethers.parseUnits("0.01", 6);
-const MIN_PROFIT = ethers.parseUnits("0.0002", 6);
+const MIN_PROFIT = ethers.parseUnits("0.0003", 6);
 
 const MIN_BATCH_PROFIT = ethers.parseUnits("0.015", 6);
 const SAFETY_MULTIPLIER = 150n;
