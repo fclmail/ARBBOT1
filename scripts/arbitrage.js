5ee29f3a744652c14ac4fc5a833a066733cc1362
fclmail <fclmail3@mail.com>
2026-04-20 09:42:54 -0400

    Decrease minimum profit threshold

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -32,7 +32,7 @@ let routerContracts;
 /* ================= CONFIG ================= */
 
 const TRADE_AMOUNT = ethers.parseUnits("0.03", 6);
-const MIN_PROFIT = ethers.parseUnits("0.0003", 6);
+const MIN_PROFIT = ethers.parseUnits("0.00003", 6);
 const MIN_BATCH_PROFIT = ethers.parseUnits("0.002", 6);
 
 const WORKER_COUNT = 32;
