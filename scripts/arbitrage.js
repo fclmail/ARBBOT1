66080af5ebf556ae30c7cb5394d78eb766a0b451
fclmail <fclmail3@mail.com>
2026-04-22 15:52:32 -0400

    Toggle RPC URLs in arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -14,8 +14,8 @@ if (!PRIVATE_KEY) throw new Error("PK missing");
 /* ================= RPC ================= */
 
 const RPCS = [
-  //"https://polygon-mainnet.core.chainstack.com/46058733cb4d6319063e68f8673791a8",
-  "https://polygon-bor-rpc.publicnode.com",
+  "https://polygon-mainnet.core.chainstack.com/46058733cb4d6319063e68f8673791a8",
+  //"https://polygon-bor-rpc.publicnode.com",
  //"https://polygon.llamarpc.com",
   //"https://polygon.drpc.org",
 //"https://polygon-public.nodies.app"
