e256e86ba339cc82a1366f36d3efde1cea20b7ca
fclmail <fclmail3@mail.com>
2026-04-22 15:54:02 -0400

    Adjust trade amount and minimum profit values

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -30,8 +30,8 @@ let routerContracts;
 
 /* ================= CONFIG ================= */
 
-const TRADE_AMOUNT = ethers.parseUnits("0.03", 6);
-const MIN_PROFIT = ethers.parseUnits("0.000003", 6);
+const TRADE_AMOUNT = ethers.parseUnits("0.02", 6);
+const MIN_PROFIT = ethers.parseUnits("0.000001", 6);
 const MIN_BATCH_PROFIT = ethers.parseUnits("0.004", 6);
 
 const WORKER_COUNT = 32;
