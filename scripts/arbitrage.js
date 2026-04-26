d4940573bf2332a03a2a42fd880d7f8e0eb66be3
fclmail <fclmail3@mail.com>
2026-04-26 15:29:58 -0400

    Fix formatting issues in arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -32,7 +32,8 @@ if (!PRIVATE_KEY) throw new Error("PK missing");
 /* ================= RPCS ================= */
 
 const RPCS = [
-  "https://polygon-mainnet.core.chainstack.com/46058733cb4d6319063e68f8673791a8"
+  "https://polygon-mainnet.core.chainstack.com/46058733cb4d6319063e68f8673791a8",
+  "https://polygon-bor-rpc.publicnode.com"
 ];
 
 let rpcIndex = 0;
