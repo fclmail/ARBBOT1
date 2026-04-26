99ded63638d4cf802d6564e47683289981cdd49a
fclmail <fclmail3@mail.com>
2026-04-26 15:47:45 -0400

    Update arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -44,9 +44,11 @@ if (!PRIVATE_KEY) throw new Error("PK missing");
 /* ================= RPC ================= */
 
 const RPCS = [
-  "https://polygon-rpc.com",
-  "https://rpc.ankr.com/polygon",
-  "https://polygon.llamarpc.com"
+  "https://polygon-mainnet.core.chainstack.com/46058733cb4d6319063e68f8673791a8",
+  "https://polygon-bor-rpc.publicnode.com"
+ // "https://polygon-rpc.com",
+ // "https://rpc.ankr.com/polygon",
+//  "https://polygon.llamarpc.com"
 ];
 
 let rpcIndex = 0;
