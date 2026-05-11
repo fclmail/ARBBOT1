98b213083ea086dc2229c02d39221e33535b9ef5
fclmail <fclmail3@mail.com>
2026-05-11 06:48:54 -0400

    Fix typo in QUICKSWAP address

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -215,7 +215,7 @@ const TOKENS = [
 /* ===================== ROUTERS ===================== */
 
 const QUICKSWAP =
-  "0xa5E0829CaCED8fFDD4De3c43696c57F7D7A678ff";
+  "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff";
 
 const SUSHISWAP =
   "0x1b02da8cb0d097eb8d57a175b88c7d8b47997506";
