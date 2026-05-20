1c68eb9fc5e31c9647da61842919e2e1961ac2c3
fclmail <fclmail3@mail.com>
2026-05-20 00:25:54 -0400

    Increase thresholds in arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -191,13 +191,13 @@ const routerContracts =
 
 const MICRO_THRESHOLD =
   ethers.parseUnits(
-    "0.000001",
+    "0.00001",
     6
   );
 
 const EXECUTION_THRESHOLD =
   ethers.parseUnits(
-    "0.000001",
+    "0.00001",
     6
   );
 
