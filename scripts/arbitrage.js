66cda0c4ce1588acb41ec921532d89c7805ab3e3
fclmail <fclmail3@mail.com>
2026-05-23 16:05:20 -0400

    Fix MIN_BATCH_PROFIT value in arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -27,7 +27,7 @@ let vault;
 
 const TRADE_AMOUNT = ethers.parseUnits("0.02", 6);
 const MIN_PROFIT = ethers.parseUnits("0.000001", 6);
-const MIN_BATCH_PROFIT = ethers.parseUnits("0.004", 6);
+const MIN_BATCH_PROFIT = ethers.parseUnits("0.0004", 6);
 
 const WORKER_COUNT = 16;
 
