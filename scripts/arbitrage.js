497031540dd8635aab84687478cdca4fc9a096f1
fclmail <fclmail3@mail.com>
2026-06-04 13:36:55 -0400

    Adjust trade parameters for arbitrage script

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -27,11 +27,11 @@ let routerContracts;
 
 /* ================= CONFIG ================= */
 
-const BASE_TRADE = ethers.parseUnits("0.04", 6);
+const BASE_TRADE = ethers.parseUnits("0.01", 6);
 const MIN_PROFIT = ethers.parseUnits("0.0002", 6);
-const GAS_COST_USDC = ethers.parseUnits("0.0003", 6);
+const GAS_COST_USDC = ethers.parseUnits("0.00003", 6);
 
-const BATCH_SIZE = 10;
+const BATCH_SIZE = 3;
 
 /* ================= CONTRACT ================= */
 
