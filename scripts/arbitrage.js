09346e9f603bd0d98e1f7930b50063e3b729c1de
fclmail <fclmail3@mail.com>
2026-06-04 12:39:43 -0400

    Increase minimum profit threshold in arbitrage script

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -27,7 +27,7 @@ let routerContracts;
 /* ================= CONFIG ================= */
 
 const BASE_TRADE = ethers.parseUnits("0.04",6);
-const MIN_PROFIT = ethers.parseUnits("0.000001",6);
+const MIN_PROFIT = ethers.parseUnits("0.0002",6);
 const GAS_COST_USDC = ethers.parseUnits("0.0003",6);
 
 const BATCH_SIZE = 10;
