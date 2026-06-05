9ce3842f18eb46a0f6475feb985567107d5fce8f
fclmail <fclmail3@mail.com>
2026-06-05 13:30:11 -0400

    Update RPC URLs in arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -14,8 +14,8 @@ if (!PRIVATE_KEY) throw new Error("PK missing");
 /* ================= RPC ================= */
 
 const RPCS = [
-   "https://polygon-bor-rpc.publicnode.com",
- //   "https://polygon-mainnet.core.chainstack.com/46058733cb4d6319063e68f8673791a8",
+  // "https://polygon-bor-rpc.publicnode.com",
+    "https://polygon-mainnet.core.chainstack.com/46058733cb4d6319063e68f8673791a8",
     // Add more RPCs for redundancy
 ];
 
