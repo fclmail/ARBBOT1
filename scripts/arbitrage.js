ec863ec0e1960daa50104123e81bbd5ac011ccf9
fclmail <fclmail3@mail.com>
2026-06-06 00:21:52 -0400

    Update arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -27,7 +27,7 @@ let routerContracts;
 /* ================= CONFIG ================= */
 
 const BASE_TRADE = ethers.parseUnits("0.01", 6);
-const MIN_PROFIT = ethers.parseUnits("0.0001", 6);
+const MIN_PROFIT = ethers.parseUnits("0.000001", 6);
 const GAS_COST_USDC = ethers.parseUnits("0.00003", 6);
 
 const BATCH_SIZE = 3
