476fbcd27a86fb2547dbbdc25fcc7d7597578774
fclmail <fclmail3@mail.com>
2026-06-10 04:43:14 -0400

    Increase BATCH_SIZE from 5 to 20

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -33,7 +33,7 @@ const BASE_TRADE = ethers.parseUnits("0.02", 6);
 const MIN_PROFIT = ethers.parseUnits("0.0002", 6);
 const GAS_COST_USDC = ethers.parseUnits("0.00003", 6);
 
-const BATCH_SIZE = 5;
+const BATCH_SIZE = 20;
 
 /* ================= GAS TOP-UP ================= */
 
