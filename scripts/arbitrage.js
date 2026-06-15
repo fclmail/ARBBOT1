0d05340e5998ca1e26b9464c2fa08134015d1a3f
fclmail <fclmail3@mail.com>
2026-06-14 23:18:13 -0400

    Fix typo in FACTORIES variable name

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -119,7 +119,7 @@ async function refreshLocalMarketCache() {
   const tokenKeys = Object.keys(TOKENS);
   localReserveCache = {};
 
-  for (const [dexName, factoryAddr] of Object.entries(FACTORORIES)) {
+  for (const [dexName, factoryAddr] of Object.entries(FACTORIES)) {
     localReserveCache[dexName] = {};
     const factoryContract = new ethers.Contract(factoryAddr, FACTORY_ABI, provider);
 
@@ -178,7 +178,7 @@ function calculatePathOutput(dexName, amountIn, path) {
    PERMUTATION MATRIX MULTI-HOP PATHFINDER
 ========================================================= */
 function findBestMultiHop() {
-  const dexList = Object.keys(FACTORORIES);
+  const dexList = Object.keys(FACTORIES);
   const intermediateTokens = Object.values(TOKENS).filter(t => t !== TOKENS.USDC);
 
   let best = null;
