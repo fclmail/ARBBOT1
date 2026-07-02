2ec5e45706ae3c4d0078adf86fff17c5c32207d4
fclmail <fclmail3@mail.com>
2026-07-02 04:47:02 -0400

    Fix DFYN address checksum in arbitrage.js

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -35,7 +35,7 @@ const CONFIG = {
     routers: {
         QUICK: ethers.getAddress("0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff"),
         SUSHI: ethers.getAddress("0x1b02da8cb0d097eb8d57a175b88c7d8b47997506"),
-        DFYN:  ethers.getAddress("0xF15361A03Eca00a63A23e1bd165157Cb02434a62")
+        DFYN:  ethers.getAddress("0xf15361a03eca00a63A23e1bd165157cb02434a62") // Checksum Error Resolved
     },
     maxPendingTransactions: 1,        
     deadlineSeconds: 45              
