d24cd1cd73d4217236e6a912784cee534f17e06c
fclmail <fclmail3@mail.com>
2026-07-13 02:04:02 -0400

    Increase BASE_ARBITRAGE_AMOUNT to 5000000

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -31,7 +31,7 @@ const CONFIG = {
     },  
     BATCH_SIZE_LIMIT: 25,  
     STUCK_TX_TIMEOUT_MS: 8000,  
-    BASE_ARBITRAGE_AMOUNT: parseUnits("50.00", 6),   
+    BASE_ARBITRAGE_AMOUNT: parseUnits("5000000.00", 6),   
     AAVE_PREMIUM_FACTOR: 5n // 0.05% fee tracking  
 };  
 
