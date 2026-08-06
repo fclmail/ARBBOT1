f2d1071a3ebb758779a3d9d68b5eb23b8535c1fe
fclmail <fclmail3@mail.com>
2026-08-06 00:56:37 -0400

    Update RPC endpoint for Polygon network

--- scripts/arbitrage.js
+++ scripts/arbitrage.js
@@ -8,8 +8,8 @@ const PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY || process.env.PRIVATE_KEY;
 if (!PRIVATE_KEY) throw new Error("PK missing");
 
 /* ================= RPC ================= */
-const RPCS = [
-"https://polygon-mainnet.public.blastapi.io"];
+const RPCS = ["https://pulsedive.com/ioc/polygon-bor-rpc.publicnode.com"
+];
 
 let rpcIndex = 0;
 let provider;
